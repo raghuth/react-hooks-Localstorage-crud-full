917b55267c38dd844f89275af8b89bb38aa9fcea
raghuth <raghavendra@rivetsys.com>
2022-03-14 10:35:02 +0530

    Added loading icon changes

--- client/src/components/list/list.js
+++ client/src/components/list/list.js
@@ -23,8 +23,9 @@ function DataList() {
       return [];
     }
   });
- 
+
   const [todo, setTodo] = useState("");
+  const [loading, setLoading] = useState(false);
   const [currentTodo, setCurrentTodo] = useState({});
   const [isEditing, setIsEditing] = useState(false);
 
@@ -35,52 +36,68 @@ function DataList() {
   function handleIInputChange(e) {
     setTodo(e.target.value);
     setCurrentTodo({ ...currentTodo, text: e.target.value });
-    console.log(currentTodo);
   }
 
   function handleFormSubmit(e) {
     e.preventDefault();
-
-    if (todo !== "") {
-      setTodoItems([
-        ...todoItems,
-        {
-          id: todoItems.length + 1,
-          text: todo.trim(),
-        },
-      ]);
+    if (  todo !== "" ) {
+      setTimeout(() => {
+        todo !== "" && setTodoItems([
+          ...todoItems,
+          {
+            id: todoItems.length + 1,
+            text: todo.trim(),
+          },
+        ]);
+        setLoading(false);
+      }, 1000);
+      setLoading(true);
     }
     // clear out the input box
     setTodo("");
+    
+    /////////////  
+    // if (localStorage.getItem("todoItems").includes(todo)) {
+    //   alert("already exists!");
+    //   return
+    // } else  {
+    //   setTimeout(() => {
+    //     todo !== "" && setTodoItems([
+    //       ...todoItems,
+    //       {
+    //         id: todoItems.length + 1,
+    //         text: todo.trim(),
+    //       },
+    //     ]);
+    //     setLoading(false);
+    //   }, 1000);
+    //   setLoading(true);
+    //   setTodo("");
+    // } 
   }
 
   const ClearAll = () => {
     localStorage.clear();
     window.location.reload();
   };
+
   function handleEditFormSubmit(e) {
     e.preventDefault();
-
-  handleUpdateTodo(currentTodo.id, currentTodo);
+    handleUpdateTodo(currentTodo.id, currentTodo);
   }
 
   function handleUpdateTodo(id, updatedTodo) {
-  
     const updatedItem = todoItems.map((todo) => {
       return todo.id === id ? updatedTodo : todo;
     });
-   
     setIsEditing(false);
-    
     setTodoItems(updatedItem);
   }
 
- function handleEditInputChange(e) {
-  setCurrentTodo({ ...currentTodo, text: e.target.value });
-  console.log(currentTodo);
-}
+  function handleEditInputChange(e) {
+    setCurrentTodo({ ...currentTodo, text: e.target.value });
+  }
   const editTodo = (todo) => {
-    console.log(" editTodo-------- id", todo);    
     setIsEditing(true);
     setCurrentTodo({ ...todo });
   };
@@ -97,83 +114,85 @@ function DataList() {
         direction="row"
         justifyContent="center"
         alignItems="center"
-        className="m-t-20"
+        className={`m-t-20 ${loading ? "is-loading" : ""}`}
       >
         <Grid item lg={4}>
-        {isEditing ? (
-      <>
-     
-       <form onSubmit={handleEditFormSubmit}>
-            <TextField
-              fullWidth
-              label="Edit todo"
-              name="editTodo"
-              value={currentTodo.text}
-              onChange={handleEditInputChange}
-              id="fullWidth"
-              variant="outlined"
-              inputProps={{
-                autoComplete: "off",
-                autoFocus: true,
-              }}
-            />
-            <Button
-              type="submit"
-              color="primary"
-              variant="contained"
-              className="m-t-20"
-            >
-             Update
-            </Button>
-            <Button
-              color="primary"
-              variant="contained"
-              className="m-t-20 m-l-20"
-              onClick={ ()=>setIsEditing(false)}
-            >
-              Cancel
-            </Button>
-          </form>
-      </>
-       
-      ) : (
-        <form onSubmit={handleFormSubmit}>
-            <TextField
-              fullWidth
-              label="Add new todo"
-              name="todo"
-              value={todo}
-              onChange={handleIInputChange}
-              id="fullWidth"
-              variant="outlined"
-              inputProps={{
-                autoComplete: "off",
-                autoFocus: true,
-              }}
-            />
-            <Button
-              type="submit"
-              color="primary"
-              variant="contained"
-              className="m-t-20"
-            >
-              Save
-            </Button>
-            <Button
-              color="primary"
-              variant="contained"
-              className="m-t-20 m-l-20"
-              onClick={ClearAll}
-            >
-              ClearAll
-            </Button>
-          </form>
-      )}
-         
+          {isEditing ? (
+            <>
+              <form onSubmit={handleEditFormSubmit}>
+                <TextField
+                  fullWidth
+                  label="Edit todo"
+                  name="editTodo"
+                  value={currentTodo.text}
+                  onChange={handleEditInputChange}
+                  id="fullWidth"
+                  variant="outlined"
+                  inputProps={{
+                    autoComplete: "off",
+                    autoFocus: true,
+                  }}
+                />
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  className="m-t-20"
+                >
+                  Update
+                </Button>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  className="m-t-20 m-l-20"
+                  onClick={() => setIsEditing(false)}
+                >
+                  Cancel
+                </Button>
+              </form>
+            </>
+          ) : (
+            <form onSubmit={handleFormSubmit}>
+              <TextField
+                fullWidth
+                label="Add new todo"
+                name="todo"
+                value={todo}
+                onChange={handleIInputChange}
+                id="fullWidth"
+                variant="outlined"
+                inputProps={{
+                  autoComplete: "off",
+                  autoFocus: true,
+                }}
+              />
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+                className="m-t-20"
+              >
+                Save
+              </Button>
+              <Button
+                color="primary"
+                variant="contained"
+                className="m-t-20 m-l-20"
+                onClick={ClearAll}
+              >
+                ClearAll
+              </Button>
+            </form>
+          )}
+
           {todoItems.length === 0 && (
             <div className="m-t-20 text-Align">No data</div>
           )}
-
+          {loading && (
+            <div className="loader">
+              <div className="icon"></div>
+            </div>
+          )}
           {todoItems.map((todo, i) => (
             <List
               sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
@@ -199,6 +218,7 @@ function DataList() {
                 <IconButton
                   edge="end"
                   aria-label="delete"
+                  className="delete-icon"
                   onClick={() => {
                     Delete(todo.id || "");
                   }}
